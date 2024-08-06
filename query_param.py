d367d8688c000a3ceaff10ad04c16a9153b266fb
Aashish Panta <aashishpanta0@gmail.com>
2024-08-06 16:48:25 -0600

    test params for stac

--- query_param.py
+++ query_param.py
@@ -167,7 +167,7 @@ def _get_cmip6_data( model, scenario, variable, quality,t1,t2,lb1,lb2,ub1,ub2):
         sys.stdout.flush()
         actual_start_date=get_actual_time(t1)
         actual_end_date=get_actual_time(t2)
-        data=_get_cmip6_data_from_stac(model, scenario, variable, actual_start_date, actual_end_date, lb, ub)
+        data=_get_cmip6_data_from_stac(model, scenario, variable, actual_start_date, actual_end_date, (lb1,lb2), (ub1,ub2))
     result = data
     return np.array(result)
 
