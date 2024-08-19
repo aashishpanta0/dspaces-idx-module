8aad5cdf69765f6c9a710b4be0ec093fee6355c0
Aashish Panta <aashishpanta0@gmail.com>
2024-08-19 15:58:04 -0600

    add idx create when not found

--- query_param.py
+++ query_param.py
@@ -4,11 +4,11 @@ import fsspec
 import pystac_client
 import numpy as np
 import os
+import concurrent.futures
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
 from bitstring import Bits, pack
 from datetime import date, timedelta,datetime
-import concurrent.futures
 
 import sys
 import OpenVisus as ov
@@ -145,39 +145,81 @@ def _get_gddp_params(name):
     return model, scenario, variable, quality,t1,t2,lb1,lb2,ub1,ub2
 
 
+def _get_idx_data(dataset_name,t1, lb1, ub1,lb2,ub2,server_location='atlantis'):
+    error_type="IDX_NOT_FOUND"
+    print(f'Looking for data at {server_location}')
+    if server_location=='atlantis':
+        db = ov.LoadDataset(f"http://atlantis.sci.utah.edu/mod_visus?dataset={dataset_name}&cached=arco")
+    
+    if server_location=='sealstorage':
+        db = ov.LoadDataset(f"http://atlantis.sci.utah.edu/mod_visus?dataset={dataset_name}&cached=arco")
+    error_type="PARAM_NOT_FOUND"
+    sys.stdout.flush()
+    print('IDX loaded...')
+    sys.stdout.flush()
+    error_type="PARAM_NOT_FOUND"
+    data=db.read(time=t1,quality=quality,x=[lb1,ub1],y=[lb2,ub2])
+    error_type="NONE"
+    return data
+
+def _create_idx_data(dataset_name, ub1,ub2, location='local'):
+    if (location=="local"):
+        db=ov.CreateIdx(
+            
+            url=f'{dataset_name}.idx',
+            fields=dataset_name,
+            time=[entire_time_start, entire_time_end, 'time_%d/'],
+            dims=[ub2,ub1],
+            arco=ARCO
+        )
+        print('New IDX created')
+    return True
+
+def _write_idx_data(dataset_url,data,time_start, time_end, lb0,lb1,ub0,ub1):
+    
+    db=ov.LoadDataset(dataset_url)
+    counter=0
+    for t in range(time_start, time_end):
+        db.write(data[counter], time=t,x=[lb1,ub1],y=[lb2,ub2])
+        counter=+1
+    print('Writing to IDX completed')
+    return True
+
+
 def _get_cmip6_data(model, scenario, variable, quality, t1, t2, lb1, lb2, ub1, ub2):
     dataset_name = f"{variable}_day_{model}_{scenario}_r1i1p1f1_gn"
     print(dataset_name)
     error_type = "NONE"
     sys.stdout.flush()
-    
     try:
         print('Checking for IDX files...')
-        error_type = "IDX_NOT_FOUND"
-        db = ov.LoadDataset(f"http://atlantis.sci.utah.edu/mod_visus?dataset={dataset_name}&cached=arco")
-        print('IDX loaded...')
-        error_type = "PARAM_NOT_FOUND"
-        sys.stdout.flush()
-        
-        def read_data_for_time_step(t):
-            return db.read(time=t, quality=quality, x=[lb1, ub1], y=[lb2, ub2])
-        
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            time_steps = range(t1, t2 + 1)
-            results = list(executor.map(read_data_for_time_step, time_steps))
-        
-        data = np.array(results)
-        error_type = "NONE"
-        
+        data = _get_idx_data(dataset_name, t1, lb1, ub1, lb2, ub2)
     except Exception as e:
-        print(f'Error with IDX file: {e}')
+        print('Error with IDX file:', e)
         print('Fetching data from Microsoft STAC now...')
         sys.stdout.flush()
-        
         actual_start_date = get_actual_time(t1)
         actual_end_date = get_actual_time(t2)
         data = _get_cmip6_data_from_stac(model, scenario, variable, actual_start_date, actual_end_date, (lb1, lb2), (ub1, ub2))
-    
+        
+        if len(data) != 0:      
+                def create_and_write_idx():
+                    try:
+                        if error_type == "IDX_NOT_FOUND":
+                            print("Starting IDX creation in the background...")
+                            test_idx_create = _create_idx_data(dataset_name, orig_ub[0], orig_ub[1] + 1)
+                            print('IDX created...')
+                            if test_idx_create:
+                                _write_idx_data(dataset_name, data, t1, t2, lb1, lb2, ub1, ub2)
+                        else:
+                            _write_idx_data(dataset_name, data, t1, t2, lb1, lb2, ub1, ub2)
+                        print('IDX creation and writing completed.')
+                    except Exception as idx_err:
+                        print('Creating or writing to IDX failed:', idx_err)
+
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    executor.submit(create_and_write_idx)
+
     return np.array(data)
 
 
@@ -192,8 +234,11 @@ if __name__ == '__main__':
     e = date(2013, 5, 2)
     start = (s - base_date).days
     span = (e - s).days
-    lb = (0,0)
-    ub = (599,1399)
+    orig_lb = (0,0)
+    orig_ub = (599,1399)
+    entire_time_start=711750
+    entire_time_end=766865
+    ARCO='2mb'
     version = pack('uint:16, uint:16', start, span).uint
     res = query(name='cmip6-planetary\\m:ACCESS-ESM1-5,v:tas', version=1, lb=lb, ub=ub)
     print(res.shape)
